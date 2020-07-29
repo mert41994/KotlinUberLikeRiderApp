a29117e430de8351b90ccdfc94685f2edc8c37c3
Emre Mert KARACA <mert41994@gmail.com>
2020-07-30 01:55:36 +0300

    Initial commit

---
+++ app/src/main/java/com/emk/kotlinuberlikeriderapp/Common/Common.kt
@@ -0,0 +1,8 @@
+package com.emk.kotlinuberlikeriderapp.Common
+
+import com.emk.kotlinuberlikeriderapp.Model.RiderModel
+
+object Common {
+    var currentRider: RiderModel? = null
+    val RIDER_INFO_REFERENCE:String = "Riders"
+}
---
+++ app/src/main/java/com/emk/kotlinuberlikeriderapp/Model/RiderModel.kt
@@ -0,0 +1,8 @@
+package com.emk.kotlinuberlikeriderapp.Model
+
+class RiderModel {
+    var firstName:String = ""
+    var lastName:String = ""
+    var phoneNumber:String = ""
+    var avatar:String = ""
+}
